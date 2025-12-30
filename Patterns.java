4001cf695553fcbc5655801899c8757d183abff7
ManeeshaNetthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2025-12-30 17:21:20 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,123 @@
+//Java program to print right angled pattern printing
+/*public class Patterns {
+    public static void main(String[] args) {
+        int num = 5;
+        for(int i = 1;i<=num;i++){
+            for(int j = 1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}*/
+/*//using scanner
+import java.util.Scanner;
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        for(int i = 1;i<=num;i++){
+            for(int j = 1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}*/
+//Inverted 
+/*public class Patterns {
+    public static void main(String[] args) {
+        for(int i = 5;i>=1;i--){
+            for(int j = 1;j<=i;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
+}*/
+/*//Using Scanner
+import java.util.Scanner;
+public class Patterns{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        for(int i = num;i>=1;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}*/
+
+//Pyramid pattern
+/*public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int s = n; s > i; s--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}*/
+//using scanner
+/*import java.util.Scanner;
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int s = n; s > i; s--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+//number pattern
+/*public class Patterns {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+//using Scanner
+/*import java.util.Scanner;
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}*/
+
+
+
+
+
+
+
